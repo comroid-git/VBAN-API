384d328e88b6c6d5b57779168afa84a760cef1b2
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2018-12-30 16:04:00 +0100

    Simple Code Reformat

--- src/main/java/de/kaleidox/vban/packet/Codec.java
+++ src/main/java/de/kaleidox/vban/packet/Codec.java
@@ -1,7 +1,7 @@
 package de.kaleidox.vban.packet;
 
 public class Codec {
-    public final static int PCM  = 0x00;
+    public final static int PCM = 0x00;
     public final static int VBCA = 0x10; // VB-Audio AOIP Codec
     public final static int VBCV = 0x20; // VB-Audio VOIP Codec
     public final static int USER = 0xF0;
