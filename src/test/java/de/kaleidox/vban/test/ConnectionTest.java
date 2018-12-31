0b3921cdcd1943298c2ebaf76493d3221240a72b
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2018-12-31 13:29:40 +0100

    Update ConnectionTest.java

--- src/test/java/de/kaleidox/vban/test/ConnectionTest.java
+++ src/test/java/de/kaleidox/vban/test/ConnectionTest.java
@@ -1,27 +1,22 @@
 package de.kaleidox.vban.test;
 
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Arrays;
 
 import de.kaleidox.vban.VBAN;
 import de.kaleidox.vban.model.DataRecievedHandler;
 
-import org.junit.Test;
-
-public class PacketSizeTest {
-    @Test
-    public void packetSizeTest() throws UnknownHostException, SocketException, InterruptedException {
+public class ConnectionTest {
+    public static void main(String[] args) throws Exception {
         VBAN<String> vban = VBAN.openTextStream(
                 new DataRecievedHandler() {
                     @Override
                     public void onDataRecieve(byte[] data) {
-                        System.out.println(data.length);
+                        System.out.println("recieved data:");
                         System.out.println(Arrays.toString(data));
                         System.out.println(new String(data));
                     }
-                }, InetAddress.getLocalHost(), VBAN.DEFAULT_PORT
+                }, InetAddress.getLocalHost(), 6981
         );
 
         vban.sendData("bus(0).mute=1");
