3ab7096e350fc1e4f706f19e1a1bd99b138b50f7
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2018-12-31 16:14:01 +0100

    Create PacketHeaderTest.java

---
+++ src/test/java/de/kaleidox/test/vban/packet/PacketHeaderTest.java
@@ -0,0 +1,32 @@
+package de.kaleidox.test.vban.packet;
+
+import de.kaleidox.vban.packet.VBANPacketHead;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PacketHeaderTest {
+    private VBANPacketHead.Factory headFactory;
+
+    @Before
+    public void setUp() {
+        headFactory = VBANPacketHead.defaultTextProtocolFactory();
+    }
+
+    @Test
+    public void testHeaderLayout() {
+        byte[] bytes = headFactory.create().getBytes();
+
+        assertEquals(bytes[0], 'V');
+        assertEquals(bytes[1], 'B');
+        assertEquals(bytes[2], 'A');
+        assertEquals(bytes[3], 'N');
+    }
+
+    @Test
+    public void testHeaderSize() {
+        assertEquals(24, headFactory.create().getBytes().length);
+    }
+}
