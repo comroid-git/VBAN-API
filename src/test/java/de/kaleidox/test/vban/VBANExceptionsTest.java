9fda443b86f20bb5807ed740c57d9a35a95cc007
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-01-01 15:58:26 +0100

    Create VBANExceptionsTest.java

---
+++ src/test/java/de/kaleidox/test/vban/VBANExceptionsTest.java
@@ -0,0 +1,43 @@
+package de.kaleidox.test.vban;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import de.kaleidox.vban.VBAN;
+import de.kaleidox.vban.packet.VBANPacket;
+
+import org.junit.Test;
+
+public class VBANExceptionsTest {
+    @Test
+    public void testStringWithinBounds() throws IOException {
+        byte[] bytes = new byte[VBANPacket.MAX_SIZE];
+        Arrays.fill(bytes, (byte) 'x');
+        VBAN<String> vban = createTextStream();
+        vban.writeFlush(bytes);
+        vban.close();
+    }
+
+    @Test(expected = IOException.class)
+    public void testStringTooLong() throws IOException {
+        byte[] bytes = new byte[VBANPacket.MAX_SIZE + 1];
+        Arrays.fill(bytes, (byte) 'x');
+        VBAN<String> vban = createTextStream();
+        vban.writeFlush(bytes);
+        vban.close();
+    }
+
+    @Test(expected = IOException.class)
+    public void testStreamClosed() throws IOException {
+        VBAN<String> vban = createTextStream();
+        vban.close();
+        vban.sendData("x");
+    }
+
+    private static VBAN<String> createTextStream() throws UnknownHostException, SocketException {
+        return VBAN.openTextStream(InetAddress.getLocalHost(), VBAN.DEFAULT_PORT);
+    }
+}
