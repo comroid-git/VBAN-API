90483c71227e755ae1539ea121d9a67d9265c0d5
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-01-14 13:53:12 +0100

    Updated JavaDocs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.kaleidox.util.model"},{"l":"de.kaleidox.vban"},{"l":"de.kaleidox.vban.model"},{"l":"de.kaleidox.vban.packet"}]
