474d707a8ee626453846d35fa65155e9d0928a82
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-01-14 13:40:35 +0100

    Added basic interfaces

---
+++ src/main/java/de/kaleidox/util/model/IntEnum.java
@@ -0,0 +1,13 @@
+package de.kaleidox.util.model;
+
+/**
+ * Interface marking an {@link Enum} as a carrier for an {@code int} value.
+ */
+public interface IntEnum {
+    /**
+     * Gets the value of the enum instance.
+     *
+     * @return The {@code int} value.
+     */
+    int getValue();
+}
