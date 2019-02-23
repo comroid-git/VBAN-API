0bfee38a19f7b1c7b30e79cda4ef2e21590eebfd
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-02-23 11:39:33 +0100

    Fixed codec subclass not being static

--- src/main/java/de/kaleidox/vban/VBAN.java
+++ src/main/java/de/kaleidox/vban/VBAN.java
@@ -313,7 +313,7 @@ public class VBAN<T> extends OutputStream {
     /**
      * Collection of codec values, required for creating a {@link VBANPacketHead.Factory}.
      */
-    public final class Codec {
+    public static final class Codec {
         public final static int PCM = 0x00;
         public final static int VBCA = 0x10; // VB-Audio AOIP Codec
         public final static int VBCV = 0x20; // VB-Audio VOIP Codec
