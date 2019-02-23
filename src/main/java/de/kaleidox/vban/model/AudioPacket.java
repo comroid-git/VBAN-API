7a1fef69d1dbbd273458a949a44840152b8d9983
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-02-23 11:39:43 +0100

    Fixed wrong javadocs

--- src/main/java/de/kaleidox/vban/model/AudioPacket.java
+++ src/main/java/de/kaleidox/vban/model/AudioPacket.java
@@ -1,7 +1,7 @@
 package de.kaleidox.vban.model;
 
 /**
- * <s>Preliminary</s> interface for packets containing audio data.
+ * Preliminary interface for packets containing audio data.
  */
 public interface AudioPacket {
 }
