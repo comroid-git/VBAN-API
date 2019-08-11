0387051459d39aaaedb0731804939ef674e8a5df
burdoto <burdoto@outlook.com>
2019-08-11 19:32:57 +0200

    Added two test classes for VBANInput and VBANOutputStreams

---
+++ src/test/java/de/kaleidox/test/vban/VBANInputStreamTest.java
@@ -0,0 +1,4 @@
+package de.kaleidox.test.vban;
+
+public class VBANInputStreamTest {
+}
--- src/test/java/de/kaleidox/test/vban/VBANOutputStreamTest.java
+++ src/test/java/de/kaleidox/test/vban/VBANOutputStreamTest.java
@@ -8,11 +8,40 @@ import java.util.Arrays;
 
 import de.kaleidox.vban.VBAN;
 import de.kaleidox.vban.VBANOutputStream;
+import de.kaleidox.vban.model.data.AudioFrame;
+import de.kaleidox.vban.model.data.MIDICommand;
 import de.kaleidox.vban.packet.VBANPacket;
 
 import org.junit.Test;
 
-public class VBANExceptionsTest {
+public class VBANOutputStreamTest {
+    @Test
+    public void testSendDataTypes() throws IOException {
+        VBANOutputStream<AudioFrame> audioFrameVBANOutputStream = VBAN.openAudioOutputStream(1);
+        audioFrameVBANOutputStream.sendData(AudioFrame.fromBytes(new byte[0]));
+        audioFrameVBANOutputStream.close();
+
+        VBANOutputStream<MIDICommand> midiCommandVBANOutputStream = VBAN.openMidiOutputStream(1);
+        midiCommandVBANOutputStream.sendData(MIDICommand.fromBytes(new byte[0]));
+        midiCommandVBANOutputStream.close();
+
+        VBANOutputStream<String> stringVBANOutputStream = VBAN.openCommandOutputStream(1);
+        stringVBANOutputStream.sendData("");
+        stringVBANOutputStream.close();
+    }
+
+    @Test(expected = Error.class)
+    public void testWriteTooLargeArray() throws IOException {
+        VBANOutputStream<String> stringVBANOutputStream = VBAN.openCommandOutputStream(1);
+
+        try {
+            stringVBANOutputStream.write(new byte[VBANPacket.MAX_SIZE_WITHOUT_HEAD + 1]);
+        } catch (IOException filter) {
+            if (filter.getMessage().startsWith("Byte array is too large, must be smaller than "))
+                throw new Error(filter);
+            throw filter;
+        }
+    }
     @Test
     public void testStringWithinBounds() throws IOException {
         byte[] bytes = new byte[VBANPacket.MAX_SIZE_WITHOUT_HEAD];
